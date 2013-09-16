1c0a3dbfbe723de4a746208c8cc4e1b9cd2c446d
matsuand <matsuand@d6661c75-0afd-443a-aae1-cb31083e41e8>
2013-09-16 03:27:44 +0000

    [LFS-SVN] bootscriptのmd5sum適正化.
    git-svn-id: svn+ssh://svn.osdn.net/svnroot/lfsbookja/trunk@813 d6661c75-0afd-443a-aae1-cb31083e41e8

---
+++ lfsbook/src/make-aux-files-ja.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -f lfs-bootscripts*.tar.bz2
+
+# Get base file name
+version=`grep "ENTITY lfs-bootscripts-version" packages.ent |cut -d'"' -f2`
+
+# Get the original bootscripts tarball
+url="http://www.linuxfromscratch.org/lfs/downloads/development/"
+wget -N "$url/lfs-bootscripts-$version.tar.bz2"
+
